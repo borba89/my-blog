a9a97e87fb2930dd1af44443e39045e0898a80a7
boris <boris@example.com>
2019-03-04 13:33:49 +0200

    blog

---
+++ testing.php
@@ -0,0 +1,21 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+$client = new  \GuzzleHttp\Client([
+    'base_url'=> 'http://localhost:8000',
+    'defaults' => [
+        'exceptions' => false,
+    ]
+]);
+
+$nickname = 'ObjectOrienter'.rand(0, 999);
+$data = array(
+'nickname' => $nickname,
+'avatarNumber' => 5,
+'tagLine' => 'a test dev!'
+);
+
+$response = $client->post('/api/post', [
+'body' => json_encode($data)
+]);
